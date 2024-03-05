abe9806d2d74960c0513e70de19a5c9796e6df47
breznicanova <breznicanova.adriana@gmail.com>
2024-03-05 07:49:07 +0100

    fix errors in class Calculator

--- javascript-learning/uloha_5_1/hw5_calculate.mjs
+++ javascript-learning/uloha_5_1/hw5_calculate.mjs
@@ -1,23 +1,8 @@
 import { Calculator } from "./hw5_calculator_oop.mjs";
 
-const sum = new Calculator(14, 41);
-console.log(`Súčet čísel ${sum.number1} a ${sum.number2}: ${sum.addition()}.`);
+const calc = new Calculator();
 
-const diff = new Calculator(174, 32);
-console.log(
-  `Rozdiel čísel ${diff.number1} a ${diff.number2}: ${diff.substraction()}.`
-);
-
-const mul = new Calculator(47, 50);
-console.log(
-  `Súčin čísel ${mul.number1} a ${mul.number2}: ${mul.multiplication()}.`
-);
-
-const dividing = new Calculator(125, 7);
-console.log(
-  `Podiel čísel ${dividing.number1} a ${
-    dividing.number2
-  }: ${dividing.division()} (po zaokrúhlení: ${
-    Math.round(dividing.division() * 10) / 10
-  }).`
-);
+const result1 = calc.addition(14, 41);
+const result2 = calc.substraction(174, 32);
+const result4 = calc.multiplication(47, 50);
+const result3 = calc.division(125, 7);
